a1c51fc58b0ced4f22d368b0f6f263ba957ea61f
mkae8 <ntemka93@gmail.com>
2024-11-06 11:36:20 +0800

    backend frontend setup

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,15 @@
 import express from "express";
 import cors from "cors";
-// import { connectDataBase } from "./src/database/config";
-
-
+import userRouter from "./router/users/userRouter";
+import { connectDataBase } from "./src/database/config";
 
 const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors());
-// connectDataBase();
+connectDataBase();
 
 app.use("/", userRouter);
-
 
 app.listen(port, () => {
   console.log(`nee deer asna --> http://localhost:${port}`);
