555d66919ee215606d4b134d73794426357837ed
mkae8 <ntemka93@gmail.com>
2024-11-06 10:49:57 +0800

    setUps

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+// import { connectDataBase } from "./src/database/config";
+
+
+
+const port = process.env.PORT;
+const app = express();
+app.use(express.json());
+app.use(cors());
+// connectDataBase();
+
+app.use("/", userRouter);
+
+
+app.listen(port, () => {
+  console.log(`nee deer asna --> http://localhost:${port}`);
+});
