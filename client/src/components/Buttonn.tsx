cebe811f74e92621e5a47458575cc6f25c12203d
24LP1665 24LP1665 <24LP1665@G302-CC0154.local>
2024-11-13 12:57:09 +0800

    xcxc

--- client/src/components/Buttonn.tsx
+++ client/src/components/Buttonn.tsx
@@ -1,41 +1,70 @@
-"use client";
-import { useEffect } from "react";
-import lottie from "lottie-web";
+// const TiltCard = () => {
+//   const ref = useRef<HTMLDivElement | null>(null);
 
-// Define the interface for component props (if any)
-const DonateButton = () => {
-  useEffect(() => {
-    const animation = lottie.loadAnimation({
-      container: document.querySelector("#lottie-animation") as Element, // Type assertion for TypeScript
-      path: "https://cdn.prod.website-files.com/650c8bf165e85f360a9c0e52/655d3846627a8e8b092ea47d_Boton_04%20sin%20fondo%204.json", // URL of the animation
-      renderer: "svg", // Use SVG renderer
-      loop: true, // Set whether it should loop
-      autoplay: false, // Do not autoplay until triggered
-    });
+//   const x = useMotionValue(0);
+//   const y = useMotionValue(0);
 
-    return () => {
-      animation.destroy(); // Clean up animation when the component is unmounted
-    };
-  }, []);
+//   const xSpring = useSpring(x);
+//   const ySpring = useSpring(y);
 
-  return (
-    <a
-      href="https://donate.territoriodezaguates.com/"
-      target="_blank"
-      className="relative inline-block w-full h-[155px]"
-    >
-      {/* The Lottie Animation */}
-      <div
-        id="lottie-animation"
-        className="absolute top-0 left-0 w-full h-full"
-      ></div>
+//   const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
 
-      {/* Button Text */}
-      <div className="relative z-10 flex justify-center items-center w-full h-full bg-regal-blue text-white text-lg font-bold uppercase px-4 py-2 rounded-md shadow-lg transition-all duration-300 ease-in-out hover:bg-light-pink">
-        Donate
-      </div>
-    </a>
-  );
-};
+//   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+//     if (!ref.current) return;
 
-export default DonateButton;
+//     const rect = ref.current.getBoundingClientRect();
+
+//     const width = rect.width;
+//     const height = rect.height;
+
+//     const mouseX = (e.clientX - rect.left) / width;
+//     const mouseY = (e.clientY - rect.top) / height;
+
+//     const rX = (mouseY - 0.5) * ROTATION_RANGE;
+//     const rY = (mouseX - 0.5) * ROTATION_RANGE;
+
+//     x.set(rX);
+//     y.set(rY);
+//   };
+
+//   const handleMouseLeave = () => {
+//     x.set(0);
+//     y.set(0);
+//   };
+
+//   return (
+//     <motion.div
+//       ref={ref}
+//       onMouseMove={handleMouseMove}
+//       onMouseLeave={handleMouseLeave}
+//       style={{
+//         transformStyle: "preserve-3d",
+//         transform,
+//       }}
+//       className="relative h-96 w-72 rounded-xl bg-gradient-to-br from-indigo-300 to-violet-300"
+//     >
+//       <div
+//         style={{
+//           transform: "translateZ(75px)",
+//           transformStyle: "preserve-3d",
+//         }}
+//         className="absolute inset-4 grid place-content-center rounded-xl bg-white shadow-lg"
+//       >
+//         <FiMousePointer
+//           style={{
+//             transform: "translateZ(75px)",
+//           }}
+//           className="mx-auto text-4xl"
+//         />
+//         <p
+//           style={{
+//             transform: "translateZ(50px)",
+//           }}
+//           className="text-center text-2xl font-bold"
+//         >
+//           HOVER ME
+//         </p>
+//       </div>
+//     </motion.div>
+//   );
+// };
--- client/src/components/petcardcomp/Petcard.tsx
+++ client/src/components/petcardcomp/Petcard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import React, { useRef } from "react";
 import {
@@ -165,12 +165,9 @@ const types: PetCategory[] = [
     imageUrl: "lizard.jpeg",
   },
 ];
-
-const ROTATION_RANGE = 32.5;
-const HALF_ROTATION_RANGE = 32.5 / 2;
-
-export default function Petcard() {
+const Petcard = () => {
   const [selectedPet, setSelectedPet] = useState<Pet | null>(null);
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   const openModal = (pet: Pet) => {
     setSelectedPet(pet);
@@ -191,117 +188,178 @@ export default function Petcard() {
     }
   };
 
+  const ref = useRef<HTMLDivElement | null>(null);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const xSpring = useSpring(x);
+  const ySpring = useSpring(y);
+
+  const ROTATION_RANGE = 5;
+  const HALF_ROTATION_RANGE = ROTATION_RANGE / 2;
+
+  const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
+
+  const handleMouseMove = (
+    e: React.MouseEvent<HTMLDivElement>,
+    index: number
+  ) => {
+    if (!ref.current) return;
+    const rect = ref.current.getBoundingClientRect();
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = (e.clientX - rect.left) / width;
+    const mouseY = (e.clientY - rect.top) / height;
+
+    const rX = (mouseY - 0.5) * ROTATION_RANGE - index;
+    const rY = (mouseX - 0.5) * ROTATION_RANGE + index;
+    x.set(rX);
+    y.set(rY);
+  };
+
+  const handleMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+  };
+
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="container mx-auto p-4">
-        <div className="flex flex-col md:flex-row justify-between items-center mt-3">
-          <h1 className="text-2xl font-bold mb-4 md:mb-0">
-            Бидний найзуудтай танилц
-          </h1>
-          <div className="flex gap-4 overflow-x-auto md:overflow-visible flex-wrap md:flex-wrap">
-            {types.map((type, index) => (
-              <div
-                key={index}
-                className="min-w-[80px] md:min-w-[100px] h-[45px] rounded-full flex items-center justify-center gap-2 cursor-pointer bg-white border hover:bg-[#F97316] transition ease-in-out duration-300 hover:text-white mb-4 md:mb-0"
-              >
-                <img
-                  className="h-[32px] w-[32px] rounded-full"
-                  src={type.imageUrl}
-                  alt=""
-                />
-                <div className="text-sm md:text-base">{type.names}</div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-8 mt-5">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {pets.slice(0, sliceCount).map((pet, index) => (
-              <div
-                key={pet.id}
-                className="cursor-pointer h-[380px] bg-white rounded-lg flex flex-col justify-between"
-                onClick={() => openModal(pet)}
-              >
-                <motion.img
-                  src={pet.imageUrl}
-                  alt={pet.name}
-                  className="w-full h-[200px] md:h-[250px] object-cover rounded-[16px] shadow-md hover:shadow-lg transition-shadow duration-300"
-                  initial={{ opacity: 0, y: 20, scale: 1 }}
-                  animate={{ opacity: 1, y: 0, rotate: 0 }}
-                  exit={{ opacity: 0, y: -20, rotate: -5 }}
-                  transition={{
-                    duration: 0.5 + (index - 0.5),
-                    ease: "easeInOut",
-                  }}
-                />
-
-                <div className="mt-2 p-4">
-                  <div className="text-xl md:text-2xl font-semibold">
-                    {pet.name}
+    <div
+      className="min-h-screen flex items-center justify-center
+    "
+    >
+      <div
+        className=" mx-auto p-4
+       "
+      >
+        <div
+          className="min-h-screen w-screen flex items-center justify-center"
+          style={{
+            backgroundImage: "url('/wallpaper3.jpeg')",
+            zIndex: -1,
+          }}
+        >
+          <div className="container mx-auto p-4">
+            <div className="flex flex-col md:flex-row justify-between items-center mt-3">
+              <h1 className="text-2xl font-bold mb-4 md:mb-0">
+                Бидний найзуудтай танилц
+              </h1>
+              <div className="flex gap-4 overflow-x-auto md:overflow-visible flex-wrap md:flex-wrap">
+                {types.map((type, index) => (
+                  <div
+                    key={index}
+                    className="min-w-[80px] md:min-w-[100px] h-[45px] rounded-full flex items-center justify-center gap-2 cursor-pointer bg-white border hover:bg-[#F97316] transition ease-in-out duration-300 hover:text-white mb-4 md:mb-0"
+                  >
+                    <img
+                      className="h-[32px] w-[32px] rounded-full"
+                      src={type.imageUrl}
+                      alt=""
+                    />
+                    <div className="text-sm md:text-base">{type.names}</div>
                   </div>
-                  <div className="text-lg md:text-xl font-normal">
-                    {pet.breed}
-                  </div>
-                  <div className="text-md md:text-lg">{pet.age}</div>
-                </div>
+                ))}
               </div>
-            ))}
-          </div>
-          <div className="group">
-            <div
-              className="relative h-12 w-40 md:h-16 md:w-48 rounded-full mt-3 text-lg md:text-xl 
-                 border border-orange-500 flex justify-center items-center overflow-hidden transition duration-300"
-              onClick={() => setSliceCount((prev) => prev + 8)}
-            >
-              <span className="relative z-10 btn_text ">See more</span>
-              <span className="absolute inset-0 bg-[#F97316] transform translate-y-full transition-transform duration-300 group-hover:translate-y-0"></span>
             </div>
+            <div className="flex flex-col items-center gap-8 mt-5">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {pets.slice(0, sliceCount).map((pet, index) => (
+                  <motion.div
+                    onMouseMove={(e) => handleMouseMove(e, index)}
+                    onMouseLeave={handleMouseLeave}
+                    ref={ref}
+                    style={{ transform, transformStyle: "preserve-3d" }}
+                    key={pet.id}
+                    className="cursor-pointer h-[380px] bg-white rounded-lg flex flex-col justify-between relative"
+                    onClick={() => openModal(pet)}
+                  >
+                    <div>
+                      <motion.img
+                        style={{
+                          transform: "translateZ(75px)",
+                        }}
+                        src={pet.imageUrl}
+                        alt={pet.name}
+                        className="w-full h-[200px] md:h-[250px] object-cover rounded-[16px] shadow-md hover:shadow-lg transition-shadow duration-300"
+                        initial={{ opacity: 0, y: 20, scale: 1 }}
+                        animate={{ opacity: 1, y: 0, rotate: 0 }}
+                        exit={{ opacity: 0, y: -20, rotate: -5 }}
+                        transition={{
+                          duration: 0.5 + (index - 0.5),
+                          ease: "easeInOut",
+                        }}
+                      />
+
+                      <div className="mt-2 p-4">
+                        <div className="text-xl md:text-2xl font-semibold">
+                          {pet.name}
+                        </div>
+                        <div className="text-lg md:text-xl font-normal">
+                          {pet.breed}
+                        </div>
+                        <div className="text-md md:text-lg">{pet.age}</div>
+                      </div>
+                    </div>
+                  </motion.div>
+                ))}
+              </div>
+              <div className="group">
+                <div
+                  className="relative h-12 w-40 md:h-16 md:w-48 rounded-full mt-3 text-lg md:text-xl 
+                 border border-orange-500 flex justify-center items-center overflow-hidden transition duration-300"
+                  onClick={() => setSliceCount((prev) => prev + 8)}
+                >
+                  <span className="relative z-10 btn_text ">See more</span>
+                  <span className="absolute inset-0 bg-[#F97316] transform translate-y-full transition-transform duration-300 group-hover:translate-y-0"></span>
+                </div>
+              </div>
+            </div>
+
+            <Dialog open={selectedPet !== null} onOpenChange={closeModal}>
+              {selectedPet && (
+                <DialogContent className="sm:max-w-[90%] md:max-w-[60%] h-[60%] p-0 md:rounded-[32px]">
+                  <motion.div
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.8 }}
+                    transition={{ duration: 0.3 }}
+                    className="flex flex-col md:flex-row"
+                  >
+                    <img
+                      src={selectedPet.imageUrl}
+                      alt={selectedPet.name}
+                      className="w-full md:w-[50%] h-[250px] md:h-auto rounded-lg shadow-md object-cover"
+                    />
+                    <div className="p-4 flex flex-col mt-4 md:mt-0 md:ml-4 justify-between items-center md:items-start">
+                      <div className="text-center md:text-left mt-6">
+                        <DialogTitle className="text-2xl md:text-3xl font-bold">
+                          {selectedPet.name}
+                        </DialogTitle>
+                        <DialogHeader>
+                          <p className="text-lg md:text-xl mt-2">
+                            {selectedPet.description}
+                          </p>
+                          <DialogDescription className="text-md md:text-lg mt-1">
+                            {selectedPet.breed} • {selectedPet.age}
+                          </DialogDescription>
+                        </DialogHeader>
+                      </div>
+                      <DialogFooter className="mt-4 md:mt-8 w-full">
+                        <Button
+                          className="w-full md:w-auto px-8 py-3 text-lg md:text-xl"
+                          onClick={handleAdoptClick}
+                        >
+                          Adopt {selectedPet.name}
+                        </Button>
+                      </DialogFooter>
+                    </div>
+                  </motion.div>
+                </DialogContent>
+              )}
+            </Dialog>
           </div>
         </div>
-
-        <Dialog open={selectedPet !== null} onOpenChange={closeModal}>
-          {selectedPet && (
-            <DialogContent className="sm:max-w-[90%] md:max-w-[60%] h-[60%] p-0 md:rounded-[32px]">
-              <motion.div
-                initial={{ opacity: 0, scale: 0.8 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0.8 }}
-                transition={{ duration: 0.3 }}
-                className="flex flex-col md:flex-row"
-              >
-                <img
-                  src={selectedPet.imageUrl}
-                  alt={selectedPet.name}
-                  className="w-full md:w-[50%] h-[250px] md:h-auto rounded-lg shadow-md object-cover"
-                />
-                <div className="p-4 flex flex-col mt-4 md:mt-0 md:ml-4 justify-between items-center md:items-start">
-                  <div className="text-center md:text-left mt-6">
-                    <DialogTitle className="text-2xl md:text-3xl font-bold">
-                      {selectedPet.name}
-                    </DialogTitle>
-                    <DialogHeader>
-                      <p className="text-lg md:text-xl mt-2">
-                        {selectedPet.description}
-                      </p>
-                      <DialogDescription className="text-md md:text-lg mt-1">
-                        {selectedPet.breed} • {selectedPet.age}
-                      </DialogDescription>
-                    </DialogHeader>
-                  </div>
-                  <DialogFooter className="mt-4 md:mt-8 w-full">
-                    <Button
-                      className="w-full md:w-auto px-8 py-3 text-lg md:text-xl"
-                      onClick={handleAdoptClick}
-                    >
-                      Adopt {selectedPet.name}
-                    </Button>
-                  </DialogFooter>
-                </div>
-              </motion.div>
-            </DialogContent>
-          )}
-        </Dialog>
       </div>
     </div>
   );
-}
+};
+
+export default Petcard;
