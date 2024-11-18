9ed56b8f92352003d7c7d7147e22f0214549f2c2
mkae8 <ntemka93@gmail.com>
2024-11-18 12:34:19 +0800

    deployment

--- server/src/database/config.ts
+++ server/src/database/config.ts
@@ -5,10 +5,15 @@ env.config();
 const URL: string = process.env.DB_URL || "";
 
 export const connectDataBase = async () => {
+  console.log(URL, "URLURLURLURL");
+  console.log(process.env, "asdkajsjdaksldmalksjhdklasjdlajsdlkasj");
+
   try {
     await connect(URL);
     console.log("holbogdoo shaas");
   } catch (err) {
+    console.log(err, "ggg");
+
     console.log("Database holboltodd aldaa garlaa");
   }
 };
