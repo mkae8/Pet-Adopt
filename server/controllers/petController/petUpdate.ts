9a74c384d177e08a674e0e56a0f708618b9b6602
24LU1697 24LU1697 <24LU1697@G302-CC0097.local>
2024-11-22 12:27:54 +0800

    done

---
+++ server/controllers/petController/petUpdate.ts
@@ -0,0 +1,61 @@
+import env from "dotenv";
+import { PetModel } from "../../src/database/models/petModel";
+env.config();
+
+export const petUpdate = async (req: any, res: any) => {
+  const userId = res.locals.userId;
+  const {
+    petName,
+    petCategoryId,
+    image,
+    age,
+    sex,
+    size,
+    weight,
+    description,
+    location,
+    status,
+    isVaccined,
+    petId,
+  } = req.body;
+  console.log(req.body);
+
+  if (
+    !petName ||
+    !petCategoryId ||
+    !image ||
+    !description ||
+    !age ||
+    !sex ||
+    !size ||
+    !weight ||
+    !location ||
+    !isVaccined ||
+    !status
+  ) {
+    return res.status(400).send({ message: "All fields are required" });
+  }
+
+  try {
+    const updatePet = await PetModel.findOneAndUpdate(
+      { _id: petId },
+      {
+        petName,
+        petCategoryId,
+        image,
+        age,
+        sex,
+        size,
+        weight,
+        description,
+        location,
+        status,
+        isVaccined,
+      }
+    );
+    res.status(201).send({ message: "Pet updated successfully", updatePet });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Failed to create pet", error });
+  }
+};
--- server/router/petAdopt/petRouter.ts
+++ server/router/petAdopt/petRouter.ts
@@ -3,6 +3,7 @@ import { fetchPets } from "../../controllers/petController/fetchPets";
 import { petCreate } from "./../../controllers/petController/petCreate";
 import { petdelete } from "./../../controllers/petController/petdelete";
 import { statusUpdate } from "./../../controllers/petController/updateStatus";
+import { petUpdate } from "./../../controllers/petController/petUpdate";
 import { Router } from "express";
 import { authMiddleware } from "../../middleware/auth";
 
@@ -12,6 +13,7 @@ petRouter.route("/get/pet").get(fetchPets);
 petRouter.route("/pet-get/:id").get(fetchpet);
 petRouter.route("/petdelete").post(petdelete);
 petRouter.route("/statusupdate").post(statusUpdate);
+petRouter.route("/update/pet").post(authMiddleware, petUpdate);
 export default petRouter;
 
 // 8000 / get / pet
